f8db17031ff84395fd100e3cebde491c23ad7770
Mary Martinez <mary.diana.martinez@gmail.com>
2022-06-16 12:20:46 -0700

    members table is added

--- sql/setup.sql
+++ sql/setup.sql
@@ -1,2 +1,18 @@
 -- Use this file to define your SQL tables
 -- The SQL in this file will be executed when you run `npm run setup-db`
+
+DROP table if exists members;
+
+CREATE table members (
+  id BIGINT GENERATED ALWAYS AS IDENTITY,
+  name VARCHAR NOT NULL,
+  nickname VARCHAR,
+  age INTEGER NOT NULL
+);
+
+INSERT INTO members (name, nickname, age) VALUES
+('Marty', null, 31),
+('Mary', null, 31),
+('Olivia', 'LiviLou', 1),
+('Melody', 'Pillowdy', 8),
+('Tempo', 'Mento', 8);
