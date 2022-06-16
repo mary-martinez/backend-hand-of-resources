811ccde64814761469494cfa8af83db02a67ee4a
Mary Martinez <mary.diana.martinez@gmail.com>
2022-06-16 12:48:13 -0700

    /members/:id test is red

--- __tests__/members.test.js
+++ __tests__/members.test.js
@@ -11,6 +11,12 @@ describe('members routes', () => {
   it('/members should return a list of member names', async () => {
     const res = await request(app).get('/members');
     expect(res.body.length).toEqual(5);
+    expect(res.body[0].name).toEqual('Marty');
+  });
+
+  it('/members/:id should return a family member with details', async () => {
+    const res = await request(app).get('/members/3');
+    expect(res.body.name).toEqual('Olivia');
   });
 
   afterAll(() => {
